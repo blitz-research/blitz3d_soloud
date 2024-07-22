89eeb75c3376b8f0da7f1b148cff19a2226f0552
Mark Sibly <marksibly@gmail.com>
2024-07-22 19:33:30 +1200

    Update sgd runtime

--- gxruntime/gxruntime_sgd.cpp
+++ gxruntime/gxruntime_sgd.cpp
@@ -1,253 +1,159 @@
 #include "std.h"
 #include "gxruntime.h"
-#include "zmouse.h"
 
 #ifndef SPI_SETMOUSESPEED
+#include "zmouse.h"
 #define SPI_SETMOUSESPEED	113
 #endif
 
-static const int static_ws = WS_VISIBLE | WS_CAPTION | WS_SYSMENU | WS_MINIMIZEBOX;
-static const int scaled_ws = WS_VISIBLE | WS_CAPTION | WS_SYSMENU | WS_SIZEBOX | WS_MINIMIZEBOX | WS_MAXIMIZEBOX;
+namespace {
 
-static string app_title;
-static string app_close;
-static gxRuntime* runtime;
-static bool busy, suspended;
-static volatile bool run_flag;
-static DDSURFACEDESC2 desktop_desc;
+	typedef int (_stdcall *LibFunc)(const void *in,	int in_sz, void *out, int out_sz);
 
-typedef int (_stdcall *LibFunc)(const void* in, int in_sz, void* out, int out_sz);
+	struct gxDll {
+		HINSTANCE hinst{};
+		map<string, LibFunc> funcs;
+	};
 
-struct gxDll
-{
-	HINSTANCE hinst;
-	map<string, LibFunc> funcs;
-};
+	enum {
+		WM_STOP = WM_APP + 1, WM_RUN, WM_END
+	};
 
-static map<string, gxDll*> libs;
+	HINSTANCE hinstance;
+	std::string cmd_line;
+	Debugger *debugger;
 
-static LRESULT CALLBACK windowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam);
+	HWND hwnd;
+	volatile bool suspended;
+	volatile bool run_flag;
 
-static Debugger* debugger;
+	gxRuntime *runtime;
 
-static set<gxTimer*> timers;
+	map<string, gxDll *> libs;
 
-enum
-{
-	WM_STOP = WM_APP + 1, WM_RUN, WM_END
-};
+	set<gxTimer *> timers;
 
-////////////////////
-// STATIC STARTUP //
-////////////////////
-gxRuntime* gxRuntime::openRuntime(HINSTANCE hinst, const string& cmd_line, Debugger* d)
-{
-	if (runtime) return 0;
+	OSVERSIONINFO osinfo;
 
-	//create debugger
-	debugger = d;
-
-	//create WNDCLASS
-	WNDCLASS wndclass;
-	memset(&wndclass, 0, sizeof(wndclass));
-	wndclass.style = CS_HREDRAW | CS_VREDRAW | CS_OWNDC;
-	wndclass.lpfnWndProc = ::windowProc;
-	wndclass.hInstance = hinst;
-	wndclass.lpszClassName = "Blitz Runtime Class";
-	wndclass.hCursor = (HCURSOR)LoadCursor(0,IDC_ARROW);
-	wndclass.hbrBackground = (HBRUSH)GetStockObject(BLACK_BRUSH);
-	RegisterClass(&wndclass);
-
-	busy = suspended = false;
-	run_flag = true;
-
-	const char* app_t = " ";
-	int ws = WS_CAPTION, ws_ex = 0;
-
-	HWND hwnd = CreateWindowEx(ws_ex, "Blitz Runtime Class", app_t, ws, 0, 0, 0, 0, 0, 0, 0, 0);
-
-	UpdateWindow(hwnd);
-
-	runtime = d_new gxRuntime(hinst, cmd_line, hwnd);
-	return runtime;
-}
-
-void gxRuntime::closeRuntime(gxRuntime* r)
-{
-	if (!runtime || runtime != r) return;
-
-	map<string, gxDll*>::const_iterator it;
-	for (it = libs.begin(); it != libs.end(); ++it)
-	{
-		FreeLibrary(it->second->hinst);
+	LRESULT CALLBACK windowProc(HWND h, UINT msg, WPARAM w, LPARAM l) {
+		if (!runtime || !run_flag) return DefWindowProc(h, msg, w, l);
+		switch (msg) {    //NOLINT
+			case WM_STOP:
+				if (!suspended) {
+					suspended = true;
+					if (debugger) debugger->debugStop();
+				}
+				return 0;
+			case WM_RUN:
+				runtime->resume();
+				return 0;
+			case WM_END:
+				debugger = 0;
+				run_flag = false;
+				return 0;
+			default:;
+		}
+		return DefWindowProc(h, msg, w, l);
 	}
-	libs.clear();
-
-	delete runtime;
-	runtime = 0;
 }
 
-//////////////////////////
-// RUNTIME CONSTRUCTION //
-//////////////////////////
-typedef int (_stdcall *SetAppCompatDataFunc)(int x, int y);
+gxRuntime *gxRuntime::openRuntime(HINSTANCE hinst, const string &cmdline, Debugger *d) {
+	if (runtime) return nullptr;
 
-gxRuntime::gxRuntime(HINSTANCE hi, const string& cl, HWND hw):
-		hinst(hi), cmd_line(cl), hwnd(hw),
-		pointer_visible(true),
-		fileSystem(0)
-{
+	::hinstance = hinst;
+	::cmd_line = cmdline;
+	::debugger = d;
+
 	CoInitialize(0);
 
 	TIMECAPS tc;
 	timeGetDevCaps(&tc, sizeof(tc));
 	timeBeginPeriod(tc.wPeriodMin);
 
+	//create dummy window for debugger to communicate with
+	WNDCLASS wndclass;
+	memset(&wndclass, 0, sizeof(wndclass));
+	wndclass.style = CS_HREDRAW | CS_VREDRAW | CS_OWNDC;
+	wndclass.lpfnWndProc = ::windowProc;
+	wndclass.hInstance = hinstance;
+	wndclass.lpszClassName = "Blitz Runtime Class";
+	wndclass.hCursor = (HCURSOR) LoadCursor(0, IDC_ARROW);
+	wndclass.hbrBackground = (HBRUSH) GetStockObject(BLACK_BRUSH);
+	RegisterClass(&wndclass);
+	hwnd = CreateWindowEx(0, "Blitz Runtime Class", " ", 0, 0, 0, 0, 0, 0, 0, 0, 0);
+
 	memset(&osinfo, 0, sizeof(osinfo));
 	osinfo.dwOSVersionInfoSize = sizeof(osinfo);
 	GetVersionEx(&osinfo);
+
+	runtime = d_new gxRuntime();
+
+	run_flag = true;
+
+	return runtime;
 }
 
-gxRuntime::~gxRuntime()
-{
-	while (timers.size()) freeTimer(*timers.begin());
+void gxRuntime::closeRuntime(gxRuntime *r) {
+	if (!runtime || runtime != r) return;
+
+	while (!timers.empty()) runtime->freeTimer(*timers.begin());
+	timers.clear();
+
+	map<string, gxDll *>::const_iterator it;
+	for (it = libs.begin(); it != libs.end(); ++it) {
+		FreeLibrary(it->second->hinst);
+	}
+	libs.clear();
+
+	delete runtime;
+	runtime = 0;
+
+	DestroyWindow(hwnd);
+	UnregisterClass("Blitz Runtime Class", hinstance);
 
 	TIMECAPS tc;
 	timeGetDevCaps(&tc, sizeof(tc));
 	timeEndPeriod(tc.wPeriodMin);
-	DestroyWindow(hwnd);
-	UnregisterClass("Blitz Runtime Class", hinst);
 
 	CoUninitialize();
 }
 
-/////////////
-// SUSPEND //
-/////////////
-void gxRuntime::suspend()
-{
-	busy = true;
-
-#if BB_BLITZ3D_ENABLED
-	pauseAudio();
-    backupGraphics();
-    unacquireInput();
-#endif
-
+void gxRuntime::suspend() {
+	if(suspended) return;
 	suspended = true;
-	busy = false;
-
 	if (debugger) debugger->debugStop();
 }
 
-////////////
-// RESUME //
-////////////
-void gxRuntime::resume()
-{
+void gxRuntime::resume() {
+	if(!suspended) return;
 	suspended = false;
-	busy = false;
-
 	if (debugger) debugger->debugRun();
 }
 
-///////////////////
-// FORCE SUSPEND //
-///////////////////
-void gxRuntime::forceSuspend()
-{
-	suspend();
-}
-
-//////////////////
-// FORCE RESUME //
-//////////////////
-void gxRuntime::forceResume()
-{
-	resume();
-}
-
-/////////////////
-// WINDOW PROC //
-/////////////////
-LRESULT gxRuntime::windowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam)
-{
-	return DefWindowProc(hwnd, msg, wparam, lparam);
-}
-
-static LRESULT CALLBACK windowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam)
-{
-	if (runtime) return runtime->windowProc(hwnd, msg, wparam, lparam);
-	return DefWindowProc(hwnd, msg, wparam, lparam);
-}
-
-//////////////////////////////
-//STOP FROM EXTERNAL SOURCE //
-//////////////////////////////
-void gxRuntime::asyncStop()
-{
+void gxRuntime::asyncStop() {
 	PostMessage(hwnd, WM_STOP, 0, 0);
 }
 
-//////////////////////////////
-//RUN  FROM EXTERNAL SOURCE //
-//////////////////////////////
-void gxRuntime::asyncRun()
-{
+void gxRuntime::asyncRun() {
 	PostMessage(hwnd, WM_RUN, 0, 0);
 }
 
-//////////////////////////////
-// END FROM EXTERNAL SOURCE //
-//////////////////////////////
-void gxRuntime::asyncEnd()
-{
+void gxRuntime::asyncEnd() {
 	PostMessage(hwnd, WM_END, 0, 0);
 }
 
-//////////
-// IDLE //
-//////////
-bool gxRuntime::idle()
-{
-	for (;;)
-	{
+bool gxRuntime::idle() {
+	while (suspended && run_flag) {
 		MSG msg;
-		if (suspended && run_flag)
-		{
-			GetMessage(&msg, 0, 0, 0);
-		}
-		else
-		{
-			if (!PeekMessage(&msg, 0, 0, 0,PM_REMOVE)) return run_flag;
-		}
-		switch (msg.message)
-		{
-			case WM_STOP:
-				if (!suspended) forceSuspend();
-				break;
-			case WM_RUN:
-				if (suspended) forceResume();
-				break;
-			case WM_END:
-				debugger = 0;
-				run_flag = false;
-				break;
-			default:
-				DispatchMessage(&msg);
-		}
+		GetMessage(&msg, 0, 0, 0);
+		DispatchMessage(&msg);
 	}
 	return run_flag;
 }
 
-///////////
-// DELAY //
-///////////
-bool gxRuntime::delay(int ms)
-{
+bool gxRuntime::delay(int ms) {
 	int t = timeGetTime() + ms;
-	for (;;)
-	{
+	for (;;) {
 		if (!idle()) return false;
 		int d = t - timeGetTime(); //how long left to wait
 		if (d <= 0) return true;
@@ -256,108 +162,61 @@ bool gxRuntime::delay(int ms)
 	}
 }
 
-///////////////
-// DEBUGSTMT //
-///////////////
-void gxRuntime::debugStmt(int pos, const char* file)
-{
+void gxRuntime::debugStmt(int pos, const char *file) {
 	if (debugger) debugger->debugStmt(pos, file);
 }
 
-///////////////
-// DEBUGSTOP //
-///////////////
-void gxRuntime::debugStop()
-{
-	if (!suspended) forceSuspend();
+void gxRuntime::debugStop() {
+	suspend();
 }
 
-////////////////
-// DEBUGENTER //
-////////////////
-void gxRuntime::debugEnter(void* frame, void* env, const char* func)
-{
+void gxRuntime::debugEnter(void *frame, void *env, const char *func) {
 	if (debugger) debugger->debugEnter(frame, env, func);
 }
 
-////////////////
-// DEBUGLEAVE //
-////////////////
-void gxRuntime::debugLeave()
-{
+void gxRuntime::debugLeave() {
 	if (debugger) debugger->debugLeave();
 }
 
-////////////////
-// DEBUGERROR //
-////////////////
-void gxRuntime::debugError(const char* t)
-{
+void gxRuntime::debugError(const char *t) {
 	if (!debugger) return;
-	Debugger* d = debugger;
+	Debugger *d = debugger;
 	asyncEnd();
-	if (!suspended)
-	{
-		forceSuspend();
-	}
+	suspend();
 	d->debugMsg(t, true);
 }
 
-///////////////
-// DEBUGINFO //
-///////////////
-void gxRuntime::debugInfo(const char* t)
-{
+void gxRuntime::debugInfo(const char *t) {
 	if (!debugger) return;
-	Debugger* d = debugger;
+	Debugger *d = debugger;
 	asyncEnd();
-	if (!suspended)
-	{
-		forceSuspend();
-	}
+	suspend();
 	d->debugMsg(t, false);
 }
 
-//////////////
-// DEBUGLOG //
-//////////////
-void gxRuntime::debugLog(const char* t)
-{
+void gxRuntime::debugLog(const char *t) {
 	if (debugger) debugger->debugLog(t);
 }
 
-/////////////////////////
-// RETURN COMMAND LINE //
-/////////////////////////
-string gxRuntime::commandLine()
-{
+string gxRuntime::commandLine() {
 	return cmd_line;
 }
 
-/////////////
-// EXECUTE //
-/////////////
-bool gxRuntime::execute(const string& cmd_line)
-{
+bool gxRuntime::execute(const string &cmd_line) {
 	if (!cmd_line.size()) return false;
 
 	//convert cmd_line to cmd and params
 	string cmd = cmd_line, params;
 	while (cmd.size() && cmd[0] == ' ') cmd = cmd.substr(1);
-	if (cmd.find('\"') == 0)
-	{
+	if (cmd.find('\"') == 0) {
 		int n = cmd.find('\"', 1);
-		if (n != string::npos)
-		{
+		if (n != string::npos) {
 			params = cmd.substr(n + 1);
 			cmd = cmd.substr(1, n - 1);
 		}
-	}
-	else
-	{
+	} else {
 		int n = cmd.find(' ');
-		if (n != string::npos)
-		{
+		if (n != string::npos) {
 			params = cmd.substr(n + 1);
 			cmd = cmd.substr(0, n);
 		}
@@ -367,188 +226,142 @@ bool gxRuntime::execute(const string& cmd_line)
 
 	SetForegroundWindow(GetDesktopWindow());
 
-	return (int)ShellExecute(GetDesktopWindow(), 0, cmd.c_str(), params.size() ? params.c_str() : 0, 0,SW_SHOW) > 32;
+	return (int) ShellExecute(GetDesktopWindow(), 0, cmd.c_str(), params.size() ? params.c_str() : 0, 0, SW_SHOW) > 32;
 }
 
-///////////////
-// APP TITLE //
-///////////////
-void gxRuntime::setTitle(const string& t, const string& e)
-{
-	app_title = t;
-	app_close = e;
-	SetWindowText(hwnd, app_title.c_str());
-}
-
-//////////////////
-// GETMILLISECS //
-//////////////////
-int gxRuntime::getMilliSecs()
-{
-	return timeGetTime();
-}
-
-/////////////////////
-// POINTER VISIBLE //
-/////////////////////
-void gxRuntime::setPointerVisible(bool vis)
-{
-	if (pointer_visible == vis) return;
-
-	pointer_visible = vis;
-
-	//force a WM_SETCURSOR
-	POINT pt;
-	GetCursorPos(&pt);
-	SetCursorPos(pt.x, pt.y);
-}
-
-gxFileSystem* gxRuntime::openFileSystem(int flags)
-{
+gxFileSystem *gxRuntime::openFileSystem(int flags) {
 	if (fileSystem) return 0;
 
 	fileSystem = d_new gxFileSystem();
 	return fileSystem;
 }
 
-void gxRuntime::closeFileSystem(gxFileSystem* f)
-{
+void gxRuntime::closeFileSystem(gxFileSystem *f) {
 	if (!fileSystem || fileSystem != f) return;
 
 	delete fileSystem;
 	fileSystem = 0;
 }
-gxTimer* gxRuntime::createTimer(int hertz)
-{
-	gxTimer* t = d_new gxTimer(this, hertz);
+
+void gxRuntime::setTitle( const string &t,const string &e ) {
+}
+
+int gxRuntime::getMilliSecs() {
+	return timeGetTime();
+}
+
+gxTimer *gxRuntime::createTimer(int hertz) {
+	gxTimer *t = d_new gxTimer(this, hertz);
 	timers.insert(t);
 	return t;
 }
 
-void gxRuntime::freeTimer(gxTimer* t)
-{
+void gxRuntime::freeTimer(gxTimer *t) {
 	if (!timers.count(t)) return;
 	timers.erase(t);
 	delete t;
 }
 
-static string toDir(string t)
-{
+static string toDir(string t) {
 	if (t.size() && t[t.size() - 1] != '\\') t += '\\';
 	return t;
 }
 
-string gxRuntime::systemProperty(const std::string& p)
-{
+string gxRuntime::systemProperty(const std::string &p) {
 	char buff[MAX_PATH + 1];
 	string t = tolower(p);
-	if (t == "cpu")
-	{
+	if (t == "cpu") {
 		return "Intel";
-	}
-	else if (t == "os")
-	{
-		switch (osinfo.dwMajorVersion)
-		{
+	} else if (t == "os") {
+		switch (osinfo.dwMajorVersion) {
 			case 3:
-				switch (osinfo.dwMinorVersion)
-				{
-					case 51: return "Windows NT 3.1";
+				switch (osinfo.dwMinorVersion) {
+					case 51:
+						return "Windows NT 3.1";
 				}
 				break;
 			case 4:
-				switch (osinfo.dwMinorVersion)
-				{
-					case 0: return "Windows 95";
-					case 10: return "Windows 98";
-					case 90: return "Windows ME";
+				switch (osinfo.dwMinorVersion) {
+					case 0:
+						return "Windows 95";
+					case 10:
+						return "Windows 98";
+					case 90:
+						return "Windows ME";
 				}
 				break;
 			case 5:
-				switch (osinfo.dwMinorVersion)
-				{
-					case 0: return "Windows 2000";
-					case 1: return "Windows XP";
-					case 2: return "Windows Server 2003";
+				switch (osinfo.dwMinorVersion) {
+					case 0:
+						return "Windows 2000";
+					case 1:
+						return "Windows XP";
+					case 2:
+						return "Windows Server 2003";
 				}
 				break;
 			case 6:
-				switch (osinfo.dwMinorVersion)
-				{
-					case 0: return "Windows Vista";
-					case 1: return "Windows 7";
+				switch (osinfo.dwMinorVersion) {
+					case 0:
+						return "Windows Vista";
+					case 1:
+						return "Windows 7";
 				}
 				break;
 		}
-	}
-	else if (t == "appdir")
-	{
-		if (GetModuleFileName(0, buff,MAX_PATH))
-		{
+	} else if (t == "appdir") {
+		if (GetModuleFileName(0, buff, MAX_PATH)) {
 			string t = buff;
 			int n = t.find_last_of('\\');
 			if (n != string::npos) t = t.substr(0, n);
 			return toDir(t);
 		}
-	}
-	else if (t == "apphwnd")
-	{
-		return itoa((int)hwnd);
-	}
-	else if (t == "apphinstance")
-	{
-		return itoa((int)hinst);
-	}
-	else if (t == "windowsdir")
-	{
-		if (GetWindowsDirectory(buff,MAX_PATH)) return toDir(buff);
-	}
-	else if (t == "systemdir")
-	{
-		if (GetSystemDirectory(buff,MAX_PATH)) return toDir(buff);
-	}
-	else if (t == "tempdir")
-	{
+	} else if (t == "apphwnd") {
+		return itoa((int) hwnd);
+	} else if (t == "apphinstance") {
+		return itoa((int) hinstance);
+	} else if (t == "windowsdir") {
+		if (GetWindowsDirectory(buff, MAX_PATH)) return toDir(buff);
+	} else if (t == "systemdir") {
+		if (GetSystemDirectory(buff, MAX_PATH)) return toDir(buff);
+	} else if (t == "tempdir") {
 		if (GetTempPath(MAX_PATH, buff)) return toDir(buff);
 	}
 	return "";
 }
 
 
-int gxRuntime::callDll(const std::string& dll, const std::string& func, const void* in, int in_sz, void* out,
-					   int out_sz)
-{
-	map<string, gxDll*>::const_iterator lib_it = libs.find(dll);
+int gxRuntime::callDll(const std::string &dll, const std::string &func, const void *in, int in_sz, void *out,
+					   int out_sz) {
+	map<string, gxDll *>::const_iterator lib_it = libs.find(dll);
 
-	if (lib_it == libs.end())
-	{
+	if (lib_it == libs.end()) {
 		HINSTANCE h = LoadLibrary(dll.c_str());
 		if (!h) return 0;
-		gxDll* t = d_new gxDll;
+		gxDll *t = d_new gxDll;
 		t->hinst = h;
 		lib_it = libs.insert(make_pair(dll, t)).first;
 	}
 
-	gxDll* t = lib_it->second;
+	gxDll *t = lib_it->second;
 	map<string, LibFunc>::const_iterator fun_it = t->funcs.find(func);
 
-	if (fun_it == t->funcs.end())
-	{
-		LibFunc f = (LibFunc)GetProcAddress(t->hinst, func.c_str());
+	if (fun_it == t->funcs.end()) {
+		LibFunc f = (LibFunc) GetProcAddress(t->hinst, func.c_str());
 		if (!f) return 0;
 		fun_it = t->funcs.insert(make_pair(func, f)).first;
 	}
 
-	static void* save_esp;
+	static void *save_esp;
 
 	_asm{
-			mov [save_esp],esp
+		mov[save_esp], esp
 	};
 
 	int n = fun_it->second(in, in_sz, out, out_sz);
 
 	_asm{
-			mov esp,[save_esp]
+		mov esp,[save_esp]
 	};
 
 	return n;
--- gxruntime/gxruntime_sgd.h
+++ gxruntime/gxruntime_sgd.h
@@ -1,4 +1,3 @@
-
 #ifndef GXRUNTIME_SGD_H
 #define GXRUNTIME_SGD_H
 
@@ -14,33 +13,8 @@
 class gxRuntime{
 public:
 
-	HWND hwnd;
-	HINSTANCE hinst;
+	gxFileSystem *fileSystem{};
 
-	gxFileSystem *fileSystem;
-
-	void moveMouse( int x,int y );
-
-	LRESULT windowProc( HWND hwnd,UINT msg,WPARAM w,LPARAM l );
-
-private:
-	gxRuntime( HINSTANCE hinst,const std::string &cmd_line,HWND hwnd );
-	~gxRuntime();
-
-	void suspend();
-	void forceSuspend();
-	void resume();
-	void forceResume();
-
-	RECT t_rect;
-	int t_style;
-	std::string cmd_line;
-	bool pointer_visible;
-	std::string app_title;
-	std::string app_close;
-
-	/***** APP INTERFACE *****/
-public:
 	static gxRuntime *openRuntime( HINSTANCE hinst,const std::string &cmd_line,Debugger *debugger );
 	static void closeRuntime( gxRuntime *runtime );
 
@@ -48,24 +22,17 @@ public:
 	void asyncRun();
 	void asyncEnd();
 
-	/***** GX INTERFACE *****/
-public:
+	void suspend();
+	void resume();
 
-	//return true if program should continue, or false for quit.
 	bool idle();
 	bool delay( int ms );
 
 	bool execute( const std::string &cmd );
-	void setTitle( const std::string &title,const std::string &close );
-	int  getMilliSecs();
-	void setPointerVisible( bool vis );
-
 	std::string commandLine();
-
 	std::string systemProperty( const std::string &t );
 
 	void debugStop();
-	void debugProfile( int per );
 	void debugStmt( int pos,const char *file );
 	void debugEnter( void *frame,void *env,const char *func );
 	void debugLeave();
@@ -76,12 +43,13 @@ public:
 	gxFileSystem *openFileSystem( int flags );
 	void closeFileSystem( gxFileSystem *filesys );
 
+	void setTitle( const std::string &t,const std::string &e );
+
+	int  getMilliSecs();
 	gxTimer *createTimer( int hertz );
 	void freeTimer( gxTimer *timer );
 
 	int callDll( const std::string &dll,const std::string &func,const void *in,int in_sz,void *out,int out_sz );
-
-	OSVERSIONINFO osinfo;
 };
 
 #endif
